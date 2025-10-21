125eac6f63f4c19440ba810a73fcb1fc8f5abea4
Jade Rosales <jaantaros@gmail.com>
2025-10-21 12:20:42 +0800

    added packages

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,16 @@
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  useEffect(() => {
+    // Hide the splash screen once your app is ready
+    // You can add any initialization logic here (fetch data, load fonts, etc.)
+    SplashScreen.hideAsync();
+  }, []);
+
   return <Stack />;
 }
